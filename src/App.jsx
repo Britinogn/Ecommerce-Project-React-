06826ce13c5e6f644fe8598e0d6d57ae82fbbf2e
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2025-10-24 17:52:38 +0100

    Move tracking to React

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes , Route} from 'react-router'
 import {HomePage} from './pages/HomePage'
 import {CheckoutPage} from './pages/CheckoutPage'
 import {OrdersPage} from './pages/OrdersPage'
+import {TrackingPage} from './pages/TrackingPage'
 
 import './App.css'
 
@@ -21,6 +22,11 @@ function App() {
       <Route 
         path='orders' 
         element={ <OrdersPage />} 
+      />
+
+      <Route 
+        path='tracking' 
+        element={ <TrackingPage />} 
       />
       
     </Routes>
